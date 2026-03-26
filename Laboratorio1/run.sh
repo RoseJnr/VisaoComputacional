5504a919ab52a0ed9002d173a305d8be35fc890f
rooseveltJr <roosevelt.junior@unifesp.br>
2026-03-26 20:06:01 -0300

    Adicionando script de compilação + execução do Laboratorio1

---
+++ Laboratorio1/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Nome do arquivo fonte e executável
+SRC="lista.cpp"
+EXEC="lista_exercicios"
+IMG="./imagem.jpg"
+
+echo "🔧 Compilando $SRC..."
+
+# Compilação
+g++ "$SRC" -o "$EXEC" `pkg-config --cflags --libs opencv4`
+
+# Verifica se compilou com sucesso
+if [ $? -ne 0 ]; then
+    echo "❌ Erro na compilação!"
+    exit 1
+fi
+
+echo "✅ Compilação concluída!"
+
+echo "🚀 Executando o programa..."
+
+# Execução
+./"$EXEC" "$IMG"
